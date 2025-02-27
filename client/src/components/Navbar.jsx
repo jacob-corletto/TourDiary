521c77b99576906d8c8421942d7ecec223e73a77
jacob-corletto <jacob.corletto@csu.fullerton.edu>
2025-02-27 14:25:59 -0800

    changed up the nav bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/react.svg"; // Ensure you have the correct path to your logo
+// import logo from "../assets/react.svg"; // Ensure you have the correct path to your logo
+// import me from "../assets/me.png"; // Ensure you have the correct path to your image
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -12,21 +13,30 @@ const Navbar = () => {
     navigate("/add-postcard");
   };
 
+  const handleProfile = () => {
+    navigate("/profile");
+  };
+
+  const isHome = window.location.pathname === "/" ? "active" : "";
+  const isCreate = window.location.pathname === "/add-postcard" ? "active" : "";
+  const isProfile = window.location.pathname === "/profile" ? "active" : "";
+
   return (
-    <nav className="bottom">
-      <button onClick={handleHome}>
+    <div className="tabs large">
+      <a className={isHome} onClick={handleHome}>
         <i>home</i>
         <span>Home</span>
-      </button>
-      <button onClick={handleCreatePostcard}>
+      </a>
+      <a className={isCreate} onClick={handleCreatePostcard}>
         <i>create</i>
         <span>Create</span>
-      </button>
-      <button>
-        <img src={logo} alt="Profile" />
+      </a>
+      <a className={isProfile} onClick={handleProfile}>
+        {/* <img className="" src={me} alt="Profile" /> */}
+        <i>person</i>
         <span>Profile</span>
-      </button>
-    </nav>
+      </a>
+    </div>
   );
 };
 
